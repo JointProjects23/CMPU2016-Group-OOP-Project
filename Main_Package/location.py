38bbbba92c1f6a6ff06a82e0768099727dede32d
haydencarroll1 <carrollhayden8@gmail.com>
2023-11-29 22:58:32 +0000

    Update location.py

--- Main_Package/location.py
+++ Main_Package/location.py
@@ -7,15 +7,6 @@ class Location:
         self.all_clues_found = False
         self.number_of_clues_to_find = number_of_clues
 
-
-class Kitchen(Location):
-    def __init__(self, number_of_clues):
-        super().__init__(number_of_clues)
-        self._visited = False
-        self._all_clues_found = False
-        self.npc = NPC("Chef", "Get out of my Kitchen", "Goes back to "
-                                                        "cooking", 50)
-
     @property
     def visited(self):
         return self._visited
@@ -38,10 +29,26 @@ class Kitchen(Location):
         else:
             print("variable is expected to be a boolean.")
 
+    def save_clues(self):
+        clues_data = {
+            "location": self.__class__.__name__,
+             "found_clues": self.found_clues,
+            "all_clues_found": self.all_clues_found
+        }
+
+
+class Kitchen(Location):
+    def __init__(self):
+        super().__init__(3)
+        self._visited = False
+        self._all_clues_found = False
+        self.npc = NPC("Chef", "Get out of my Kitchen", "Goes back to "
+                                                        "cooking", 50)
+
 
 class Library(Location):
-    def __init__(self, number_of_clues):
-        super().__init__(number_of_clues)
+    def __init__(self):
+        super().__init__(5)
         self.visited = False
         self.all_clues_found = False
         self.npc = NPC("Librarian", "SHHHH", "Goes back to "
@@ -49,8 +56,8 @@ class Library(Location):
 
 
 class Attic(Location):
-    def __init__(self, number_of_clues):
-        super().__init__(number_of_clues)
+    def __init__(self):
+        super().__init__(2)
         self.visited = False
         self.all_clues_found = False
         self.npc = NPC("Anne", "Hello", "Goes back to "
