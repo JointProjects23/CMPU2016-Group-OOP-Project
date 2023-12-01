0f63be2cfc5430b2709e9fad37ea7c585dd9a484
Sam2004-2 <83888868+Sam2004-2@users.noreply.github.com>
2023-12-01 14:08:50 +0000

    Update main.py
    Fix leaderboard logic for printing the leaderboard out at the end of the game

--- Main_Package/main.py
+++ Main_Package/main.py
@@ -9,10 +9,9 @@ if __name__ == "__main__":
     game.run()
     game_leaderboard = Leaderboard() # creates a new empty instance of
     # leaderboard class
-    game_leaderboard = game_leaderboard.load_leaderboard(
-        "user_data.json") # saves the file to the leaderboard instance
+    game_leaderboard = game_leaderboard.load_leaderboard("user_data.json")  # saves the file to the leaderboard instance
 
-    top_players = game_leaderboard.get_top_players() # gets top players from
+    top_players = game_leaderboard.get_top_players()  # gets top players from
     # leaderboard, saved as dictionary
     for player, score in top_players:
 
