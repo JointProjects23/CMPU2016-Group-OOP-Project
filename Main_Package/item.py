18e05848db1ede9295f9acdadc743ed009a9df97
haydencarroll1 <carrollhayden8@gmail.com>
2023-12-03 22:36:27 +0000

    made items more modular

--- Main_Package/item.py
+++ Main_Package/item.py
@@ -17,7 +17,7 @@ class Item:
     The Item class represents an item that can impact the game.
     """
 
-    def __init__(self, name, description):
+    def __init__(self, name, description, impact, score_increase):
         """
         Initialize an Item with a name and description.
 
@@ -26,6 +26,8 @@ class Item:
         """
         self.name = name
         self.description = description
+        self.impact = impact
+        self.score_increase = score_increase
 
     def use(self, game):
         """
@@ -33,17 +35,7 @@ class Item:
 
         :param game: The game instance on which the item is used.
         """
-        if self.name == "Clue from Suspect":
-            print("Using the clue from the suspect reveals additional information.")
-            game.score += 5
-            game.suspect_confession = True
-        elif self.name == "NPC Interaction":
-            print("Using the information from NPCs helps you uncover a secret.")
-            game.score += 3
-            game.uncover_secret = True
-        elif self.name == "Torn Fabric":
-            print("Using the torn fabric clue helps you reconstruct the sequence of events.")
-            game.score += 2
-            game.reconstruct_sequence = True
-        else:
-            print(f"Using the {self.name} doesn't seem to have any significant impact.")
+
+        print(self.impact)
+        game.score += self.score_increase
+
